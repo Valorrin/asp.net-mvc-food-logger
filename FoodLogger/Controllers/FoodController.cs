c794b46124cc66958ebd80da5cdc22185539cfce
Valentin Popov <valorrin@gmail.com>
2023-05-15 12:58:09 +0300

    Added food list view

--- FoodLogger/Controllers/FoodController.cs
+++ FoodLogger/Controllers/FoodController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FoodLogger.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FoodLogger.Controllers
 {
     public class FoodController : Controller
     {
+        private readonly ApplicationDbContext context;
+
+        public FoodController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            var foods = context.Foods.ToList();
+            return View(foods);
         }
     }
 }
