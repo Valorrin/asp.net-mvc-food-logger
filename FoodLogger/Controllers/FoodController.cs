1d6b9e5d0985552072bf8a3de350dafbb5e56bf8
Valentin Popov <valorrin@gmail.com>
2023-05-15 12:19:09 +0300

    Added FoodController

---
+++ FoodLogger/Controllers/FoodController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FoodLogger.Controllers
+{
+    public class FoodController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
