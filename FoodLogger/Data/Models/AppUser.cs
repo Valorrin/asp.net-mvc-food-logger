e5db5c0f42939af9a8f57044d8a0834cb513164c
Valentin Popov <valorrin@gmail.com>
2023-06-16 19:00:12 +0300

    Added Identity

---
+++ FoodLogger/Data/Models/AppUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace FoodLogger.Data.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public string Name { get; set; }
+        public ICollection<Food> Foods { get; set; }
+    }
+}
--- FoodLogger/Program.cs
+++ FoodLogger/Program.cs
@@ -1,6 +1,10 @@
 using FoodLogger.Data;
+using FoodLogger.Data.Migrations;
+using FoodLogger.Data.Models;
 using FoodLogger.Interfaces;
 using FoodLogger.Repository;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +16,13 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddIdentity<AppUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddMemoryCache();
+builder.Services.AddSession();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+       .AddCookie();
+
 
 var app = builder.Build();
 
