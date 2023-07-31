231d0c96e45014536835a2f9c0f597c7e59d43e3
Valentin Popov <valorrin@gmail.com>
2023-07-31 17:15:02 +0300

    Added LoginController

---
+++ FoodLogger/Controllers/AccountController.cs
@@ -0,0 +1,56 @@
+﻿using FoodLogger.Data;
+using FoodLogger.Data.Models;
+using FoodLogger.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodLogger.Controllers
+{
+    public class AccountController: Controller
+    {
+        private readonly UserManager<AppUser> userManager;
+        private readonly SignInManager<AppUser> signInManager;
+        private readonly ApplicationDbContext context;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, ApplicationDbContext context)
+        {
+            this.context = context;
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        public IActionResult Login()
+        {
+            var response = new LoginViewModel();
+            return View(response);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Login(LoginViewModel loginViewModel) 
+        {
+            if (!ModelState.IsValid) return View(loginViewModel);
+
+            var user = await userManager.FindByEmailAsync(loginViewModel.Email);
+
+            if (user != null) 
+            {
+                //User is found, check password
+                var passwordCheck = await userManager.CheckPasswordAsync(user, loginViewModel.Password);
+                if (passwordCheck)
+                {
+                    var result = await signInManager.PasswordSignInAsync(user, loginViewModel.Password, false, false);
+                    if (result.Succeeded) 
+                    {
+                        return RedirectToAction("Index", "Race");
+                    }
+                }
+                //Incorect Password
+                TempData["Error"] = "Wrong credentials. Please, try again";
+                return View(loginViewModel);
+            }
+            //User not found
+            TempData["Error"] = "Wrong credentials. Please, try again";
+            return View(loginViewModel);
+        }
+    }
+}
---
+++ FoodLogger/Models/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.PortableExecutable;
+
+namespace FoodLogger.Models
+{
+    public class LoginViewModel
+    {
+        [Display(Name = "Emal Address")]
+        [Required(ErrorMessage = "Email address is required")]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
